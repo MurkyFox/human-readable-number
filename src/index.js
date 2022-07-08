af21b16a8d326e4de657002ebf2944c69f21d9bc
MurkyFox <gwy113@gmail.com>
2022-07-08 20:30:36 +0300

    fix: reduce code

--- src/index.js
+++ src/index.js
@@ -2,35 +2,23 @@ module.exports = function toReadable (number) {
     const humanNumber = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'hundred'];
     const a = number.toString().split('');
     if (number <= 20) {
-        return humanNumber[number];       
+        return humanNumber[number];
     } else if (20 < number && number < 30){
-        return `${humanNumber[20]} ${humanNumber[a[1]]}`;    
-    } else if (number === 30){
-        return humanNumber[21];
+        return `${humanNumber[20]} ${humanNumber[a[1]]}`;
+    } else if (number === Number(a[0] + '0')){
+        return `${humanNumber[Number('2' + (a[0] - 2))]}`;
     } else if (30 < number && number < 40){
         return `${humanNumber[21]} ${humanNumber[a[1]]}`;
-    } else if (number === 40){
-        return humanNumber[22];
     } else if (40 < number && number < 50){
         return `${humanNumber[22]} ${humanNumber[a[1]]}`;
-    } else if (number === 50){
-        return humanNumber[23];
     } else if (50 < number && number < 60){
         return `${humanNumber[23]} ${humanNumber[a[1]]}`;
-    } else if (number === 60){
-        return humanNumber[24];
     } else if (60 < number && number < 70){
         return `${humanNumber[24]} ${humanNumber[a[1]]}`;
-    } else if (number === 70){
-        return humanNumber[25];
     } else if (70 < number && number < 80){
         return `${humanNumber[25]} ${humanNumber[a[1]]}`;
-    } else if (number === 80){
-        return humanNumber[26];
     } else if (80 < number && number < 90){
         return `${humanNumber[26]} ${humanNumber[a[1]]}`;
-    } else if (number === 90){
-        return humanNumber[27];
     } else if (90 < number && number < 100){
         return `${humanNumber[27]} ${humanNumber[a[1]]}`;
     } else if (number === Number(a[0] + '00')){
